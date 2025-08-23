47c6ff5258c2afcab42bfc89703f4f0969ffa30c
arturgorski14 <arturgorski14@gmail.com>
2025-08-23 22:39:35 +0200

    test: GetPerson red

--- app/application/queries.py
+++ app/application/queries.py
@@ -4,3 +4,9 @@ from dataclasses import dataclass
 @dataclass
 class GetAllPeopleQuery:
     pass  # no fields needed since we want all users
+
+
+@dataclass
+class GetPersonQuery:
+    uid: str
+    name: str
---
+++ tests/api/test_get_person.py
@@ -0,0 +1,40 @@
+import pytest
+from starlette import status
+
+from app.application.bus import QueryBus
+from app.application.queries import GetPersonQuery
+from app.bootstrap import get_query_bus
+from app.main import app
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_get_nonexistent_person(client): ...
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_get_person_with_invalid_uid(): ...
+
+
+def test_get_person(client):
+    # Arrange
+    uid = "123"
+
+    class FakeHandler:
+        def handle(self, query):
+            return [
+                {"uid": uid, "name": "Alice"},
+            ]
+
+    fake_bus = QueryBus()
+    fake_bus.register(GetPersonQuery, FakeHandler())
+
+    app.dependency_overrides[get_query_bus] = lambda: fake_bus
+
+    # Act
+    response = client.get(f"/people/{uid}")
+
+    # Assert
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == [
+        {"uid": {uid}, "name": "Alice"},
+    ]
