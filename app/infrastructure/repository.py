c932ef490ca9eddc83fc92a4b68f61a362780974
arturgorski14 <arturgorski14@gmail.com>
2025-08-24 15:16:03 +0200

    refactor: repository.py 100% coverage

--- app/infrastructure/repository.py
+++ app/infrastructure/repository.py
@@ -44,14 +44,12 @@ class FakePersonRepository(PersonRepositoryInterface):
 
     def assert_called_once_with(self, method_name: str, *args, **kwargs) -> None:
         matching_calls = [c for c in self._calls if c[0] == method_name]
-        if len(matching_calls) != 1:
-            raise AssertionError(
-                f"Expected {method_name} to be called once. "
-                f"Called {len(matching_calls)} times."
-            )
+        assert len(matching_calls) == 1, (
+            f"Expected {method_name} to be called once."
+            f"Called {len(matching_calls)} times."
+        )
         called_args, called_kwargs = matching_calls[0][1], matching_calls[0][2]
-        if called_args != args or called_kwargs != kwargs:
-            raise AssertionError(
-                f"{method_name} called with {called_args}, {called_kwargs}, "
-                f"expected {args}, {kwargs}"
-            )
+        assert called_args == args and called_kwargs == kwargs, (
+            f"{method_name} called with {called_args}, {called_kwargs},"
+            f"expected {args}, {kwargs}"
+        )
