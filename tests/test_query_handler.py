48b87b9f509ade496298b23cfefe4386f4517f9b
arturgorski14 <arturgorski14@gmail.com>
2025-08-24 11:17:18 +0200

    test: query_handler improved

--- tests/test_query_handler.py
+++ tests/test_query_handler.py
@@ -6,22 +6,28 @@ from tests.fakes import FakeRepository
 
 
 def test_get_all_people_handler_delegates():
+    # Arrange
     repo = FakeRepository()
     handler = GetAllPeopleHandler(repo)
     query = GetAllPeopleQuery()
 
+    # Act
     handler.handle(query)
 
+    # Assert
     assert repo._called_method == "get_all"
 
 
 @pytest.mark.parametrize("uid", ["1", "non-existent"])
 def test_get_person_handler_delegates(uid):
+    # Arrange
     repo = FakeRepository()
     handler = GetPersonHandler(repo)
     query = GetPersonQuery(uid)
 
+    # Act
     handler.handle(query)
 
+    # Assert
     assert repo._called_method == "get"
     assert repo._called_args == uid
