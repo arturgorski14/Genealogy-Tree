91ed71d38ab62b6f41a8cb9e79476b193df9180b
arturgorski14 <arturgorski14@gmail.com>
2025-09-02 19:50:22 +0200

    test: test_command_handler -> green

--- tests/test_command_handler.py
+++ tests/test_command_handler.py
@@ -1,5 +1,5 @@
-from app.application.command_handlers import CreatePersonHandler
-from app.application.commands import CreatePersonCommand
+from app.application.command_handlers import CreatePersonHandler, DeletePersonHandler
+from app.application.commands import CreatePersonCommand, DeletePersonCommand
 
 
 def test_create_person_handler_delegates(person_repo):
@@ -15,4 +15,14 @@ def test_create_person_handler_delegates(person_repo):
     person_repo.create.assert_called_once_with(**payload)
 
 
-# TODO: test for delete command handler
+def test_delete_person_handler_delegates(person_repo):
+    # Arrange
+    payload = {"uid": "fake-uid"}
+    handler = DeletePersonHandler(person_repo)
+    command = DeletePersonCommand
+
+    # Act
+    handler.handle(command(**payload))
+
+    # Assert
+    person_repo.delete.assert_called_once_with(**payload)
